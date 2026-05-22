73f593c5d6bb10ffd14d68efb64346701ed62db9
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2026-05-22 18:01:31 +0700

    Use official alpine docker image

--- build-edge.sh
+++ build-edge.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 ARCH=$(uname -m)
 case "$ARCH" in
-    x86_64) ARCH=x86_64 ;;
-    amd64) ARCH=x86_64 ;;
-    aarch64) ARCH=aarch64 ;;
-    arm64) ARCH=aarch64 ;;
+    x86_64) ARCH=amd64 ;;
+    amd64) ARCH=amd64 ;;
+    aarch64) ARCH=arm64 ;;
+    arm64) ARCH=arm64 ;;
     *)
         echo "Unsupported architecture: $ARCH"
         exit 1
